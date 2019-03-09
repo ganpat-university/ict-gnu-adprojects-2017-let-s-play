d83038c6a30105d73a1f79512ae57ab270882d78
trisha16 <trisha.cba1722@ict.gnu.ac.in>
2019-03-09 19:29:37 +0530

    Sign Up Added

--- app/src/main/java/com/example/lets_play/MainActivity.java
+++ app/src/main/java/com/example/lets_play/MainActivity.java
@@ -1,19 +1,33 @@
 package com.example.lets_play;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
-
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {            //To check if user has logged in or not
+             if(firebaseAuth.getCurrentUser()== null){
+                Intent loginIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(loginIntent);
+            }
+        }
     }
 
     @Override
